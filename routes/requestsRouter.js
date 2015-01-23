57fda7bb9447ea768ab1a594e805e53e9f609505
Chris Anderson <chris.anderson@byu.net>
2015-01-22 17:17:23 -0700

    fix race condition in service request.

--- routes/requestsRouter.js
+++ routes/requestsRouter.js
@@ -3,34 +3,66 @@
 */
 
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 var Request = require('../models/request');
 var Service = require('../models/service');
 
-// TODO: this is supposed to require an API key
-router.post('/', function saveRequest(req, res, next) {
-	var requestedDate = new Date().toISOString();
+router.get('/:requestID.json', queryStatus);
+router.get('/', findRequests);
+router.post('/', validatePOSTParameters, saveRequest);
+
+module.exports = router;
+
+function queryStatus(req, res){
+	var requestID = req.body.service_request_id;
+	var query = Request.find({service_request_id : requestID});
+	query.exec(function foundRequest(error, result){
+		if (error) {
+			res.send500('There was an error querying the request status.');
+		} else {
+			res.send(cleanUpGetResponse(result));
+		}
+	});
+}
+
+function findRequests(req, res) {
+	var requestsQuery = Request.buildQuery(req.body);
+	requestsQuery.exec(function foundRequests(error, results){
+		if (error) {
+			res.send500('There was an error while searching for your request.');
+		} else {
+			res.send(results.slice(0,999).map(cleanUpGetResponse));
+		}
+	});
+}
+
+function validatePOSTParameters(req, res, next) {
 	var serviceCode = req.body.service_code;
 
 	if (typeof serviceCode === 'undefined') {
 		res.send400('A service code was not defined.');
 		return;
 	}
-
-	Service.checkExistence(serviceCode, function (err, serviceExists){
-		if (err) {
-			res.send500('Something went wrong while trying to see if you had a valid service code.');
-		}
-		if (!serviceExists){
-			res.send404('Couldn\'t find a service of that type. Wrong service code.');
-		}
-		return;
-	});
-
 	if (!hasLocationInfo(req.body)) {
 		res.send400('Your request must have one of the following: latitude and longitude, address_string, or address_id.');
 		return;
 	}
+
+	Service.checkExistence(serviceCode, function (err, serviceExists){
+		if (err) {
+			res.send500('Something went wrong while trying to see if you had a valid service code.');
+			return;
+		}
+		if (!serviceExists){
+			res.send404('Couldn\'t find a service of that type. Wrong service code.');
+			return;
+		} 
+		next();
+	});
+}
+
+function saveRequest(req, res, next) {
+	var requestedDate = new Date().toISOString();
 
 	var request = new Request({
 		account_id:         req.body.account_id,
@@ -63,33 +95,7 @@ router.post('/', function saveRequest(req, res, next) {
 			res.send('Request not saved.');
 		}
 	});
-});
-
-router.get('/', function findRequests(req, res) {
-	var requestsQuery = Request.buildQuery(req.body);
-	requestsQuery.exec(function foundRequests(error, results){
-		if (error) {
-			res.send500('There was an error while searching for your request.');
-		} else {
-			res.send(results.slice(0,999).map(cleanUpGetResponse));
-		}
-	});
-});
-
-// Should be /requests/:id.json
-router.get('/:requestID.json', function queryStatus(req, res){
-	var requestID = req.body.service_request_id;
-	var query = Request.find({service_request_id : requestID});
-	query.exec(function foundRequest(error, result){
-		if (error) {
-			res.send500('There was an error querying the request status.')
-		} else {
-			res.send(cleanUpGetResponse(result));
-		}
-	});
-});
-
-module.exports = router;
+}
 
 //  required for requests
 function hasLocationInfo(params) {
