03b882bfe6d01bdea5b4d3583c750dd16681c7cd
Chris Anderson <chris.anderson@byu.net>
2015-02-14 12:54:47 -0700

    fixing loadDB file

---
+++ loaddb2.js
@@ -0,0 +1,33 @@
+var mongoose   = require('mongoose');
+var Request = require('./models/request');
+var Service = require('./models/service');
+
+mongoose.connect('mongodb://localhost/city-issues');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function dbConnected() {
+    console.log('DB connected.');
+});
+
+new Service({
+    description:  'Requests to fix potholes',
+    metadata:     false,
+    keywords:     'roads',
+    group:        'infrastructure',
+    service_code: 1,
+    service_name: 'potholes',
+    type:  'realtime'
+}).save(function (){
+        console.log('Saved.');
+    });
+
+new Request({account_id: 123, address_string: 'address', address_id: 4, device_id: 5, description: 'desc', email: 'email', first_name: 'Chris', last_name: 'Anderson', lat: '2432', long: '2342342', media_url: undefined, phone: '24232234', service_code: 1, requested_datetime: new Date().toISOString(), status: 'open'})
+    .save(function (err, request){
+        console.log('saved');
+    });
+new Request({account_id: 123, address_string: 'address', address_id: 4, device_id: 5, description: 'desc', email: 'email', first_name: 'Chris', last_name: 'Anderson', lat: '2432', long: '2342342', media_url: undefined, phone: '24232234', service_code: 2, requested_datetime: new Date().toISOString(), status: 'open'})
+    .save(function (err, request){
+        console.log('saved');
+    });
+
+//change in the file
